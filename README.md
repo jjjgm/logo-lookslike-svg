9c406d7bf3153dc5bc0bb702a9b13dd26fb2d003
jjjgm <businessjgm@icloud.com>
2023-04-05 09:48:30 -0700

    added to read me

--- README.md
+++ README.md
@@ -5,19 +5,31 @@ building a command line SVG file (logo generator) with NodeJS
 Create a SVG file that will generate a logo based on the given responses from the user. The logo will be created through the command line using NodeJS.
 
 ## Table of Contents
-[Installation]
-[Usage]
-[Credits]
-[Contrbutions]
-[License]
-[Assets]
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Credits](#credits)
+
+[Contrbutions](#contributions)
+
+[License](#license)
+
+[Assets](#assets)
 
 ## Installation
+NodeJS, Inquirer@8.2.4, Jest, and SVG modules
+
 
 ## Usage
 
 ## Credits
 [SVG DOM](https://github.com/svgdotjs/svgdom)
+[ECMAScript Modules](https://jestjs.io/docs/ecmascript-modules)
+[SVG Texts](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Texts)
+[SVG Basic Shapes](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes)
+[SVG- Getting Started](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Getting_Started)
+
 
 ## Testing
 [!Video Submission Link]()
--- index.js
+++ index.js
@@ -1,95 +1,95 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const {Circle, Triangle, Square} = require('./lib/shapes');
-// const generateMarkdown = require('./shapes');
-// console.log(generateMarkdown);
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const {Circle, Triangle, Square} = require('./lib/shapes');
+// // const generateMarkdown = require('./shapes');
+// // console.log(generateMarkdown);
 
-class Svg {
-    constructor() {
-        this.textElement =''
-        this.shapeElement = '';
-    }
-    render() {
-        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>`;
-    }
+// class Svg {
+//     constructor() {
+//         this.textElement =''
+//         this.shapeElement = '';
+//     }
+//     render() {
+//         return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>`;
+//     }
 
-    setShapeElement(shape) {
-        this.shapeElement=shape.render();
-    }
+//     setShapeElement(shape) {
+//         this.shapeElement=shape.render();
+//     }
 
-    setTextElement(text, color) {
-        if (text.length > 3) {
-            throw new Error("Text must not exceed 3 characters.");
-        }
-        this.textElement = `<text x="10" y="10"></text>`
-    }
-}
+//     setTextElement(text, color) {
+//         if (text.length > 3) {
+//             throw new Error("Text must not exceed 3 characters.");
+//         }
+//         this.textElement = `<text x="10" y="10"></text>`
+//     }
+// }
 
-let questions = [
-    {
-        type: 'list',
-        message: 'What shape would you like to start with?',
-        name: 'shape',
-        choices: ['Circle', 'Square', 'Triangle'],
+// let questions = [
+//     {
+//         type: 'list',
+//         message: 'What shape would you like to start with?',
+//         name: 'shape',
+//         choices: ['Circle', 'Square', 'Triangle'],
         
-    }
-    ,
-    {
-        type: 'list',
-        message: 'What size would you like the shape to be?',
-        name:'size',
-        choices: ['Small', 'Medium', 'Large'],
-    }
-    ,
-    {
-        type: 'input',
-        message: 'What color would you like the shape to be?',
-        name: 'color',
-    }
-    ,
-    {
-        type: 'input',
-        message: 'What text do you want inside the shape? Limit to 3 characters!',
-        name: 'text',
-    }
-    ,
-    {
-        type: 'input',
-        message: 'What color would like for the text?',
-        name: 'textColor',
-    }
-]
+//     }
+//     ,
+//     {
+//         type: 'list',
+//         message: 'What size would you like the shape to be?',
+//         name:'size',
+//         choices: ['Small', 'Medium', 'Large'],
+//     }
+//     ,
+//     {
+//         type: 'input',
+//         message: 'What color would you like the shape to be?',
+//         name: 'color',
+//     }
+//     ,
+//     {
+//         type: 'input',
+//         message: 'What text do you want inside the shape? Limit to 3 characters!',
+//         name: 'text',
+//     }
+//     ,
+//     {
+//         type: 'input',
+//         message: 'What color would like for the text?',
+//         name: 'textColor',
+//     }
+// ]
 
-// function to write in user responses for later use
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Your selections have been saved!');
-    });
-}
+// // function to write in user responses for later use
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) => {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log('Your selections have been saved!');
+//     });
+// }
 
-// function to initialize creation of logo
-function init() {
-    let answers = inquirer.prompt(questions);
-
-
+// // function to initialize creation of logo
 // function init() {
-//     inquirer.prompt(questions)
-//     .then((answers) => {
-//             const markdown = generateMarkdown(answers);
-//             console.log(answers)
-//             writeToFile('logo.txt', logo);
-//         })
-}
+//     let answers = inquirer.prompt(questions);
+
+
+// // function init() {
+// //     inquirer.prompt(questions)
+// //     .then((answers) => {
+// //             const markdown = generateMarkdown(answers);
+// //             console.log(answers)
+// //             writeToFile('logo.txt', logo);
+// //         })
+// }
 
 
 
 
 
-// const App = require ('./lib/app');
+// // const App = require ('./lib/app');
 
-// const app = new App ();
+// // const app = new App ();
 
-// app.run ();
+// // app.run ();
