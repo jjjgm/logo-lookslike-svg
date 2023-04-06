d4a29729a0afeb8e5740541c033c93f848aaed83
jjjgm <businessjgm@icloud.com>
2023-04-06 02:07:24 -0700

    need this last one

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
 const inquirer = require('inquirer');
+// const path = require('path');
 const fs = require('fs');
-const {Circle, Triangle, Square} = require('./lib/shapes');
-const SVG  = require('./lib/svg');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const { SVG } = require('svg.js');
+
 const Shape = require('./lib/shapes');
 
-const generateSvg = require('./generateSvg');
 
+function promptUser() {
+    inquirer.prompt(
+        [
+            {
+                type: 'list',
+                message: 'What shape would you like to start with?',
+                name: 'shape',
+                choices: ['Circle', 'Square', 'Triangle'],
 
+            }
+            ,
+            {
+                type: 'list',
+                message: 'What size would you like the shape to be?',
+                name: 'size',
+                choices: ['Small', 'Medium', 'Large'],
+            }
+            ,
+            {
+                type: 'input',
+                message: 'What color would you like the shape to be?',
+                name: 'color',
+            }
+            ,
+            {
+                type: 'input',
+                message: 'What text do you want inside the shape? Limit to 3 characters!',
+                name: 'text',
+            }
+            ,
+            {
+                type: 'input',
+                message: 'What color would like for the text?',
+                name: 'textColor',
+            }
+        ]).then((answers) => {
+            let shape = null; // initialize shape to null
 
-let questions = [
-    {
-        type: 'list',
-        message: 'What shape would you like to start with?',
-        name: 'shape',
-        choices: ['Circle', 'Square', 'Triangle'],
-        
-    }
-    ,
-    {
-        type: 'list',
-        message: 'What size would you like the shape to be?',
-        name:'size',
-        choices: ['Small', 'Medium', 'Large'],
-    }
-    ,
-    {
-        type: 'input',
-        message: 'What color would you like the shape to be?',
-        name: 'color',
-    }
-    ,
-    {
-        type: 'input',
-        message: 'What text do you want inside the shape? Limit to 3 characters!',
-        name: 'text',
-    }
-    ,
-    {
-        type: 'input',
-        message: 'What color would like for the text?',
-        name: 'textColor',
-    }
-]
+            switch (answers.shape) {
+                // Create the shape object based on user input
+            }
 
-// function to write in user responses for later use
+            if (shape !== null) {
+                shape.setColor(answers.color);
+                const svg = SVG().addTo('body', '100%').size(500, 500);
+                svg.add(shape.getShape());
+                if (answers.text.length > 3) {
+                    console.log('Please enter text limited to 3 characters');
+                } else {
+                    svg.text(answers.text).fill(answers.textColor).move(50, 50);
+                    const mysvg = svg.svg();
+                    writeToFile('mylogo', mysvg);
+                }
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
+    const filePath = `./examples/${fileName}.svg`;
+    fs.writeFile(filePath, data, (err) => {
         if (err) {
             return console.log(err);
         }
-        console.log('Your selections have been saved!');
+        console.log(`Your ${fileName} logo has been created`);
     });
 }
 
-// function to initialize creation of logo
-function init() {
-    let answers = inquirer.prompt(questions);
-
-
-// function init() {
-//     inquirer.prompt(questions)
-//     .then((answers) => {
-//             const markdown = generateMarkdown(answers);
-//             console.log(answers)
-//             writeToFile('logo.txt', logo);
-//         })
-}
-
-
-
-
-
-// // const App = require ('./lib/app');
-
-// // const app = new App ();
-
-// // app.run ();
-
-
+promptUser();
