045988a559af0d078dab2bcfed24564460ea65f8
jjjgm <businessjgm@icloud.com>
2023-04-06 12:18:11 -0700

    finished generator

--- index.js
+++ index.js
@@ -1,80 +1,71 @@
 const inquirer = require('inquirer');
-// const path = require('path');
+const path = require('path');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
-const { SVG } = require('svg.js');
+const  SVG  = require('./lib/svg');
 
-const Shape = require('./lib/shapes');
+function run() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What shape would you like to start with?',
+            name: 'shape',
+            choices: ['Circle', 'Square', 'Triangle'],
+        },
+        {
+            type: 'input',
+            message: 'What color would you like the shape to be?',
+            name: 'color',
+        },
+        {
+            type: 'input',
+            message: 'What text do you want inside the shape? Limit to 3 characters!',
+            name: 'text',
+        },
+        {
+            type: 'input',
+            message: 'What color would like for the text?',
+            name: 'textColor',
+        },
+    ]).then((answers) => {
+        const shapeClassName = answers.shape;
+        const shapeColor = answers.color;
+        const text = answers.text;
+        const textColor = answers.textColor;
 
+        const svg = new SVG();
 
-function promptUser() {
-    inquirer.prompt(
-        [
-            {
-                type: 'list',
-                message: 'What shape would you like to start with?',
-                name: 'shape',
-                choices: ['Circle', 'Square', 'Triangle'],
-
-            }
-            ,
-            {
-                type: 'list',
-                message: 'What size would you like the shape to be?',
-                name: 'size',
-                choices: ['Small', 'Medium', 'Large'],
-            }
-            ,
-            {
-                type: 'input',
-                message: 'What color would you like the shape to be?',
-                name: 'color',
-            }
-            ,
-            {
-                type: 'input',
-                message: 'What text do you want inside the shape? Limit to 3 characters!',
-                name: 'text',
-            }
-            ,
-            {
-                type: 'input',
-                message: 'What color would like for the text?',
-                name: 'textColor',
-            }
-        ]).then((answers) => {
-            let shape = null; // initialize shape to null
-
-            switch (answers.shape) {
-                // Create the shape object based on user input
-            }
-
-            if (shape !== null) {
-                shape.setColor(answers.color);
-                const svg = SVG().addTo('body', '100%').size(500, 500);
-                svg.add(shape.getShape());
-                if (answers.text.length > 3) {
-                    console.log('Please enter text limited to 3 characters');
-                } else {
-                    svg.text(answers.text).fill(answers.textColor).move(50, 50);
-                    const mysvg = svg.svg();
-                    writeToFile('mylogo', mysvg);
-                }
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-}
-
-function writeToFile(fileName, data) {
-    const filePath = `./examples/${fileName}.svg`;
-    fs.writeFile(filePath, data, (err) => {
-        if (err) {
-            return console.log(err);
+        let shape;
+        switch (shapeClassName) {
+            case 'Circle':
+                shape = new Circle({color: shapeColor});
+                break;
+            case 'Square':
+                shape = new Square({color: shapeColor});
+                break;
+            case 'Triangle':
+                shape = new Triangle({color: shapeColor});
+                break;
         }
-        console.log(`Your ${fileName} logo has been created`);
+        shape.setColor(shapeColor);
+        svg.setShape(shape);
+        svg.setText(text, textColor);
+
+        const dirName = 'examples';
+        if (!fs.existsSync(dirName)) {
+            fs.mkdirSync(dirName);
+        }
+
+        const fileName = path.join(dirName, 'logo.svg');
+        const content = svg.render();
+        fs.writeFile(fileName, content, function (error) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(`Generated ${fileName}`);
+        });
     });
 }
 
-promptUser();
+run();
+
